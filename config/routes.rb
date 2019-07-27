65cb0be4c5e234e278f7deab5d2b7c59e0e7d8cb
Sara Trice <sara@saratrice.com>
2019-07-27 14:39:55 -0500

    Add dashboard, OmniAuth error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'dashboard' => 'dashboard#show'
 
   get 'auth/oauth2/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
